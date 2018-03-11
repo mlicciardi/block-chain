0357fea4aaf3feaccccfbea069a4c307e76089ae
mlicciardi <mattia.licciardi@gmail.com>
2018-03-11 19:06:32 +0000

    is-valid-chain

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ class BlockChain{
     this.chain.push(block);
   }
 
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) return false;
+
+      if (currentBlock.previousHash !== previousBlock.hash) return false;
+    }
+
+    return true;
+  }
+
   getLatestBlock() {
     return this.chain[this.chain.length - 1];
   }
