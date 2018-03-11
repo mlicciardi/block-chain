9f4a047d1790f20e0cb7bc2a693ad3fc386168f7
mlicciardi <mattia.licciardi@gmail.com>
2018-03-11 18:51:36 +0000

    block-class

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict'
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = '';
+    }
+}
